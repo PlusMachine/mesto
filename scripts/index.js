bae73f0c832058a581b1b59748e8b311fcd4b019
Artem Movchan <artyom.movchan@gmail.com>
2023-03-14 11:51:23 -0300

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,28 @@
 const popupElement = document.querySelector(".popup");
 const popupCloseButtonElement = popupElement.querySelector(".popup__close-button");
 const popupEditButtonElement = document.querySelector(".profile__edit-button");
-const formElement = popupElement.querySelector(".popup__container");
-const nameInput = formElement.querySelector(".popup__input-name");
-const jobInput = formElement.querySelector(".popup__input-job");
+const formElement = popupElement.querySelector(".popup__form");
+const nameInput = formElement.querySelector(".popup_input_name");
+const jobInput = formElement.querySelector(".popup_input_job");
 const nameElement = document.querySelector(".profile__name");
 const jobElement = document.querySelector(".profile__profession");
 
 const openPopup = function () {
   popupElement.classList.add("popup_is-opened");
+  nameInput.value = nameElement.textContent;
+  jobInput.value = jobElement.textContent;
 };
 
 const closePopup = function () {
   popupElement.classList.remove("popup_is-opened");
 };
 
-const closePopupByClickOnOverlay = function (event) {
+/*const closePopupByClickOnOverlay = function (event) {
   if (event.target !== event.currentTarget) {
     return;
   }
   closePopup();
-};
-
-nameInput.value = nameElement.textContent;
-jobInput.value = jobElement.textContent;
+};*/
 
 
 function handleFormSubmit(evt) {
@@ -33,10 +32,9 @@ function handleFormSubmit(evt) {
   closePopup();
 };
 
-
 popupEditButtonElement.addEventListener("click", openPopup);
 popupCloseButtonElement.addEventListener("click", closePopup);
-popupElement.addEventListener("click", closePopupByClickOnOverlay);
+/*popupElement.addEventListener("click", closePopupByClickOnOverlay);*/
 formElement.addEventListener("submit", handleFormSubmit);
 
 
